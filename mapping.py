8a7c90e5ab08f8c514f4b80cf9d0a826557a0e04
n1ckeeel <67nikhilbahadur@gmail.com>
2026-05-23 02:00:51 +0100

    access character_

--- mapping.py
+++ mapping.py
@@ -1,11 +1,16 @@
+import json
+import os
+
 class MorseMapping:
     def __init__(self):
-        self.encoded = {}
+        path = os.path.join(os.path.dirname(__file__), "character_set.json")
+        with open(path, "r") as f:
+            self.encoded = json.load(f)
+
         self.decoded = {v: k for k, v in self.encoded.items()}
-    
+
     def to_morse(self, char):
-        return self.encoded.get(char.upper())
-        
+        return self.encoded.get(char.upper(), "")
+
     def to_char(self, code):
-        return self.decoded.get(code)
-        
+        return self.decoded.get(code, "")
